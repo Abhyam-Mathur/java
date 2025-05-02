c86f6fd5819bad8b52d306cabf0587256ee7155c
Abhyam-Mathur <154632064+Abhyam-Mathur@users.noreply.github.com>
2025-05-02 20:28:20 +0530

    Add files via upload

---
+++ StudentManagementSystem/Main.java
@@ -0,0 +1,14 @@
+package StudentManagementSystem;
+
+import StudentRecords.Student;
+
+public class Main {
+    public static void main(String[] args) {
+        Student student1 = new Student(101, "Aarav Sharma", 'A');
+        Student student2 = new Student(102, "Priya Verma", 'B');
+
+        student1.displayStudentInfo();
+        System.out.println();
+        student2.displayStudentInfo();
+    }
+}
