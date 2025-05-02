ad1e7957775d545a8c5f0c64689796b42aca5e2d
Abhyam-Mathur <154632064+Abhyam-Mathur@users.noreply.github.com>
2025-05-02 20:27:53 +0530

    Add files via upload

---
+++ p/A.java
@@ -0,0 +1,23 @@
+package p;
+
+public class A {
+    public void publicMethod() {
+        System.out.println("Public method accessed.");
+    }
+
+    protected void protectedMethod() {
+        System.out.println("Protected method accessed.");
+    }
+
+    void defaultMethod() {
+        System.out.println("Default method accessed.");
+    }
+
+    private void privateMethod() {
+        System.out.println("Private method accessed.");
+    }
+
+    public void accessPrivateMethod() {
+        privateMethod(); // Private method can be called inside the same class
+    }
+}
